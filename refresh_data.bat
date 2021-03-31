1b48471dc70f182bd7454bc4f1b3e100efb9e00e
Aleksander Molak <aleksander.molak@gmail.com>
2021-03-31 20:57:37 +0200

    Minor batch file bug fix

--- refresh_data.bat
+++ refresh_data.bat
@@ -4,8 +4,8 @@ echo Welcome to MAAS - Data Refresher
 echo Loading...
 echo.
 call activate.bat MAAS
-@REM python.exe scripts/tools/refresh_data.py
-python.exe scripts/tools/update_pdf_data.py
+python.exe scripts/tools/refresh_data.py
+@REM python.exe scripts/tools/update_pdf_data.py
 echo Done
 echo Thank you!
 echo.
